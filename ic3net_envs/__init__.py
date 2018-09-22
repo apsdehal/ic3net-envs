1762ae542851a111513f999740cbfbc94ecf57f6
Amanpreet Singh <asg@fb.com>
2018-09-22 13:21:06 -0700

    Edit entry point for envs

--- ic3net_envs/__init__.py
+++ ic3net_envs/__init__.py
@@ -2,10 +2,10 @@ from gym.envs.registration import register
 
 register(
     id='PredatorPrey-v0',
-    entry_point='gccnet_envs.predator_prey_env:PredatorPreyEnv',
+    entry_point='ic3net_envs.predator_prey_env:PredatorPreyEnv',
 )
 
 register(
     id='TrafficJunction-v0',
-    entry_point='gccnet_envs.traffic_junction_env:TrafficJunctionEnv',
+    entry_point='ic3net_envs.traffic_junction_env:TrafficJunctionEnv',
 )
